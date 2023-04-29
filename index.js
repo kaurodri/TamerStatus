834fe8b3b81453eec8faae411c1e9a2330c03317
HieA1lpha <engkauanrodrigues@gmail.com>
2023-04-29 05:35:49 -0300

    2023-04-29
    Reposição do Map status. Ainda faltando alguns ajustes na legenda.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var amizade = 35, TStatus = [], TAplicado = [];
+var amizade = 20, TStatus = [], TAplicado = [];
 
 {
     let categorias = ['AT', 'DEF', 'HP', 'DS'], banco = [];
@@ -9,11 +9,11 @@ var amizade = 35, TStatus = [], TAplicado = [];
         c++;
     };
 
-    var status = new Map(banco);
+    var statusT = new Map(banco);
 }
 
 {
-    var classes = ['status', 'slots', 'aplica', 'equipamento1', 'equipamento2']
+    var classes = ['status', 'passivo', 'slots', 'aplica', 'equipamento1', 'equipamento2']
     let categorias = classes, banco = [];
 
     let c = 0;
@@ -38,12 +38,12 @@ var amizade = 35, TStatus = [], TAplicado = [];
 }
 
 {
-    let chapeuSt = [true, 2, 100, 300],
-        oculosSt = [],
-        blusaSt = [],
-        shortSt = [],
-        luvaSt = [],
-        botaSt = [true, 1, 115, 3],
+    let chapeuSt = [],
+        oculosSt = [true, 0, 1, 100, 176],
+        blusaSt = [true, 0, 2, 55, 450, 436],
+        shortSt = [true, 0, 2, 55, 148, 141],
+        luvaSt = [true, 210, 2, 130, 252, 257],
+        botaSt = [true, 30, 1, 115, 3],
         suitSt = [];
 
     const legenda = {
@@ -72,5 +72,71 @@ var amizade = 35, TStatus = [], TAplicado = [];
         }
     }
 }
-console.log(chapeu);
-console.log(bota);
+//console.log(luva);
+//console.log(bota);
+
+{
+    if (luva.get(classes[0]) == true) {
+        if (luva.get(classes[2]) == 2) {
+
+            let e1 = Math.trunc((luva.get(classes[3]) * luva.get(classes[4])) / 100),
+            e2 = Math.trunc((luva.get(classes[3]) * luva.get(classes[5])) / 100),
+            
+            tot = e1 + e2 + luva.get(classes[1]),
+            
+            tot1 = tot +129,
+            
+            tot2 = tot1 * amizade / 100;
+            
+
+            //console.log(status.set('AT', tot1));
+            //console.log(status.set('AT', tot2));
+        }
+    }
+}
+
+{
+    let categorias = ['AT', 'DEF', 'HP', 'DS']
+
+    let c = 0;
+    let ss = todos.length;
+    while (c < 1) {
+
+        const legenda = {
+            'chapeu' : categorias[3],
+            'oculos': categorias[3],
+            'blusa': categorias[2],
+            'short': categorias[1],
+            'luva': categorias[0],
+            'bota': null,
+            'suit': null
+        }
+
+        /*
+        let somaSlot1 = Math.trunc((todos[c].get(classes[3]) * todos[c].get(classes[4])) / 100),
+        somaSlot2 = Math.trunc((todos[c].get(classes[3]) * todos[c].get(classes[4])) / 100),
+        somaSlots = somaSlot1 + somaSlot2;
+
+        // var todos = [chapeu, oculos, blusa, short, luva, bota, suit];
+
+        
+
+        
+        if (todos[c].get(classes[0]) == true) {
+            if (legenda[todos[c]] != null) {
+                statusT.set(legenda[todos[c]], somaSlots);
+                c++;
+            } else {
+                c++;
+            }
+        } else {
+            console.log('esse é false');
+            c++;
+        }
+        /*/
+        console.log(todos[1].toString());
+        c++
+
+    }
+    //console.log(statusT);
+}
